0d17d8fb2762238edff682cb26147b99cea6835e
user1 <bekleman@gmail.com>
2022-05-04 11:42:14 +0300

    more asciinema records about added to README.md; isPrime game added.

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ import hexlet.code.games.Even;
 import hexlet.code.games.Calc;
 import hexlet.code.games.GCD;
 import hexlet.code.games.Progression;
+import hexlet.code.games.Prime;
 
 import java.util.Scanner;
 
@@ -12,12 +13,14 @@ public class App {
     public static final int CALC_NM = 3;
     public static final int GDC_NM = 4;
     public static final int PROGRESSION_NM = 5;
+    public static final int PRIME_NM = 6;
     public static final String MENU_INFO = "Please enter the game number and press Enter.";
     public static final String MENU_GAMES = GREET_NM + " - Greet\n"
             + EVEN_NM + " - Even\n"
             + CALC_NM + " - Calc\n"
             + GDC_NM + " - GCD\n"
             + PROGRESSION_NM + " - Progression\n"
+            + PRIME_NM + " - Prime\n"
             + "0 - Exit\nYour choice: ";
 
 
@@ -36,6 +39,7 @@ public class App {
             case CALC_NM -> Calc.playGame();
             case GDC_NM -> GCD.playGame();
             case PROGRESSION_NM -> Progression.playGame();
+            case PRIME_NM -> Prime.playGame();
             default -> {
             }
         }
